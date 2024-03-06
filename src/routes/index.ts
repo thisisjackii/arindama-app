ea02b2309350355a6b0807044d4a738cf125545a
thisisjackii <jaxsix06@gmail.com>
2024-03-06 12:21:57 +0700

    Making routes

---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import authRoutes from './authRoutes';
+import goalRoutes from './goalRoutes';
+import mlRoutes from './mlRoutes';
+import moneyRoutes from './moneyRoutes';
+import timeRoutes from './timeRoutes';
+// Import other route files as needed
+
+const routes = [authRoutes, goalRoutes, mlRoutes, moneyRoutes, timeRoutes];
+
+export default routes;
