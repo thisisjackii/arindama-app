35b9d0b69f919e7c625fa3e7de7cf68ea55f9d0c
thisisjackii <jaxsix06@gmail.com>
2024-03-06 12:26:41 +0700

    Modifying server run message

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-// src/index.ts
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import chalk from "chalk";
+import routes from "./routes";
 
 dotenv.config();
 
@@ -11,6 +12,22 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
+app.use(express.json());
+app.use('/api', routes);
+
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  const message = `Server is running at http://localhost:${port}`;
+  const messageLength = message.length;
+  const boxWidth = messageLength + 6;
+  const horizontalLine = chalk.yellow("=".repeat(boxWidth));
+  const emptyLine = chalk.yellow("||") + " ".repeat(boxWidth - 4) + chalk.yellow("||");
+  const messageLine = chalk.yellow("||") + " " + chalk.blue(message) + " " + chalk.yellow("||");
+
+  console.log(horizontalLine);
+  console.log(emptyLine);
+  console.log(messageLine);
+  console.log(emptyLine);
+  console.log(horizontalLine);
 });
+
+export default app;
